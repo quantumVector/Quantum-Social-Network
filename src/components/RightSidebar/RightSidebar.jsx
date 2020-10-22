88b19744294da0135d29ced3a93de9b0346b67cb
quantumVector <payned@mail.ru>
2020-10-22 15:42:21 +0300

    Добавлена верстка Friends online и изменён размер тайтлов

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -4,6 +4,12 @@ import Ashley from '../../assets/profiles/Ashley-mini.png';
 import Liara from '../../assets/profiles/Liara-mini.png';
 import Mordin from '../../assets/profiles/Mordin-mini.png';
 import James from '../../assets/profiles/James-mini.png';
+import Anderson from '../../assets/profiles/Anderson-mini.png';
+import Garrus from '../../assets/profiles/Garrus-mini.png';
+import Miranda from '../../assets/profiles/Miranda-mini.png';
+import Tali from '../../assets/profiles/Tali-mini.png';
+import Thane from '../../assets/profiles/Thane-mini.png';
+import Urdnot from '../../assets/profiles/Urdnot-mini.png';
 
 const RightSidebar = () => {
   return (
@@ -11,7 +17,6 @@ const RightSidebar = () => {
       <div className={classes.stories}>
         <div className={classes.title}>STORIES</div>
         <div className={classes.items}>
-
           <div className={classes.item}>
             <img src={Ashley} className={classes.storiesIcon} alt='icon'></img>
             <div className={classes.personInfo}>
@@ -43,8 +48,91 @@ const RightSidebar = () => {
               <div className={classes.time}>18 hours ago</div>
             </div>
           </div>
+        </div>
+      </div>
 
+      <div className={classes.friends}>
+        <div className={classes.title}>10 FRIENDS ONLINE</div>
+        <div className={classes.items}>
+          <div className={classes.item}>
+            <img src={Anderson} className={classes.profileIcon} alt='icon'></img>
+            <div className={classes.personInfo}>
+              <div className={classes.name}>David Anderson</div>
+              <div className={classes.online}></div>
+            </div>
+          </div>
 
+          <div className={classes.item}>
+            <img src={Ashley} className={classes.profileIcon} alt='icon'></img>
+            <div className={classes.personInfo}>
+              <div className={classes.name}>Ashley Williams</div>
+              <div className={classes.online}></div>
+            </div>
+          </div>
+
+          <div className={classes.item}>
+            <img src={Garrus} className={classes.profileIcon} alt='icon'></img>
+            <div className={classes.personInfo}>
+              <div className={classes.name}>Garrus Vakarian</div>
+              <div className={classes.online}></div>
+            </div>
+          </div>
+
+          <div className={classes.item}>
+            <img src={James} className={classes.profileIcon} alt='icon'></img>
+            <div className={classes.personInfo}>
+              <div className={classes.name}>James Vega</div>
+              <div className={classes.online}></div>
+            </div>
+          </div>
+
+          <div className={classes.item}>
+            <img src={Liara} className={classes.profileIcon} alt='icon'></img>
+            <div className={classes.personInfo}>
+              <div className={classes.name}>Liara T'Soni</div>
+              <div className={classes.online}></div>
+            </div>
+          </div>
+
+          <div className={classes.item}>
+            <img src={Miranda} className={classes.profileIcon} alt='icon'></img>
+            <div className={classes.personInfo}>
+              <div className={classes.name}>Miranda Lawson</div>
+              <div className={classes.online}></div>
+            </div>
+          </div>
+
+          <div className={classes.item}>
+            <img src={Mordin} className={classes.profileIcon} alt='icon'></img>
+            <div className={classes.personInfo}>
+              <div className={classes.name}>Mordin Solus</div>
+              <div className={classes.online}></div>
+            </div>
+          </div>
+
+          <div className={classes.item}>
+            <img src={Tali} className={classes.profileIcon} alt='icon'></img>
+            <div className={classes.personInfo}>
+              <div className={classes.name}>Tali'Zorah</div>
+              <div className={classes.online}></div>
+            </div>
+          </div>
+
+          <div className={classes.item}>
+            <img src={Thane} className={classes.profileIcon} alt='icon'></img>
+            <div className={classes.personInfo}>
+              <div className={classes.name}>Thane Krios</div>
+              <div className={classes.online}></div>
+            </div>
+          </div>
+
+          <div className={classes.item}>
+            <img src={Urdnot} className={classes.profileIcon} alt='icon'></img>
+            <div className={classes.personInfo}>
+              <div className={classes.name}>Urdnot Wrex</div>
+              <div className={classes.online}></div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
