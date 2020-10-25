7c4ba5859bd11f82b46a98559503c043337bef01
quantumVector <payned@mail.ru>
2020-10-25 15:27:40 +0300

    Добавлена верстка FollowingBlock

--- src/components/FollowingBlock/FollowingBlock.jsx
+++ src/components/FollowingBlock/FollowingBlock.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
 import classes from './FollowingBlock.module.css';
+import actions from '../../assets/post-actions-icon.png';
+import spectres from '../../assets/spectres-icon.png';
+import alliance from '../../assets/alliance-icon.png';
+import news from '../../assets/galaxy-news-icon.png';
 
 const FollowingBlock = () => {
   return (
-    <div className={classes.block}></div>
+    <div className={classes.block}>
+      <div className={classes.blockHeader}>
+        <div className={classes.title}>FOLLOWING</div>
+        <img src={actions} className={classes.actions} alt="icon"></img>
+      </div>
+        <div className={classes.items}>
+          <div className={classes.item}>
+            <img src={spectres} className={classes.icon} alt='icon'></img>
+            <div className={classes.info}>Organization SPECTRES</div>
+          </div>
+        </div>
+
+        <div className={classes.items}>
+          <div className={classes.item}>
+            <img src={alliance} className={classes.icon} alt='icon'></img>
+            <div className={classes.info}>Alliance Special Forces</div>
+          </div>
+        </div>
+
+        <div className={classes.items}>
+          <div className={classes.item}>
+            <img src={news} className={classes.icon} alt='icon'></img>
+            <div className={classes.info}>Galactic News</div>
+          </div>
+        </div>
+    </div>
   )
 }
 
