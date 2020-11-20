496a65346f788b4d63d1c97e0a29da46d775891b
quantumVector <payned@mail.ru>
2020-11-20 12:15:16 +0300

    Ссылки изменены на NavLink в MenuItem и добавлены стили для кнопок

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from './MenuItem.module.css';
 
 const MenuItem = (props) => {
   return (
     <li className={classes.item} href={props.href}>
-      <div className={classes.iconCont}>
-        <img src={props.icon} className={classes.icon} alt='icon'></img>
-      </div>
-      <a href={props.href}>
+      <NavLink to={props.href} activeClassName={classes.active}>
+        <div className={classes.iconCont}>
+          <img src={props.icon} className={classes.icon} alt='icon'></img>
+        </div>
         <div className={classes.text}>{props.label}</div>
-      </a>
+      </NavLink>
     </li>
   )
 }
