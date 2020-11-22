d70629b2a54f007802884e7942da0426da5b4515
quantumVector <payned@mail.ru>
2020-11-22 12:11:21 +0300

    Создана отдельная папка данных для чата

--- src/state/state.js
+++ src/state/state.js
@@ -16,6 +16,7 @@ import photoEvent from '../assets/event.jpg';
 import photo1Post1 from '../assets/postPhotos/photo1.jpg';
 import photo2Post1 from '../assets/postPhotos/photo2.jpg';
 import photo3Post1 from '../assets/postPhotos/photo3.jpg';
+import chatAshleyWilliams from './chat/AshleyWilliams';
 
 const state = {
   profile: {
@@ -121,126 +122,7 @@ const state = {
   },
 
   chat: {
-    AshleyWilliams: {
-      messages: [
-        {
-          from: 'friend',
-          photo: photoAshley,
-          time: '10:05 PM',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ornare.',
-        },
-        {
-          from: 'me',
-          photo: photoShepard,
-          time: '10:25 PM',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quis purus sapien. Nullam id ligula sem. Proin vitae risus rutrum.',
-        },
-        {
-          from: 'friend',
-          photo: photoAshley,
-          time: '10:26 PM',
-          text: 'Lorem ipsum dolor sit amet.',
-        },
-        {
-          from: 'me',
-          photo: photoShepard,
-          time: '10:47 PM',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id ultrices ligula, et placerat libero. Suspendisse potenti. Nam non commodo.',
-        },
-        {
-          from: 'friend',
-          photo: photoAshley,
-          time: '10:48 PM',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget imperdiet ante. Aliquam quis tortor.',
-        },
-        {
-          from: 'friend',
-          photo: photoAshley,
-          time: '10:49 PM',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultricies ex eget ligula auctor, non tempor massa sodales. Ut rutrum elit id accumsan bibendum. Vivamus at tincidunt eros, sed porttitor est. Curabitur ultricies congue aliquam.',
-        },
-        {
-          from: 'me',
-          photo: photoShepard,
-          time: '11:05 PM',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare luctus dui.',
-        },
-        {
-          from: 'friend',
-          photo: photoAshley,
-          time: '11:07 PM',
-          text: 'Lorem ipsum dolor.',
-        },
-        {
-          from: 'friend',
-          photo: photoAshley,
-          time: '10:05 PM',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ornare.',
-        },
-        {
-          from: 'me',
-          photo: photoShepard,
-          time: '10:25 PM',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quis purus sapien. Nullam id ligula sem. Proin vitae risus rutrum.',
-        },
-        {
-          from: 'friend',
-          photo: photoAshley,
-          time: '10:26 PM',
-          text: 'Lorem ipsum dolor sit amet.',
-        },
-        {
-          from: 'me',
-          photo: photoShepard,
-          time: '10:47 PM',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id ultrices ligula, et placerat libero. Suspendisse potenti. Nam non commodo.',
-        },
-        {
-          from: 'friend',
-          photo: photoAshley,
-          time: '10:48 PM',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget imperdiet ante. Aliquam quis tortor.',
-        },
-        {
-          from: 'friend',
-          photo: photoAshley,
-          time: '10:49 PM',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ultricies ex eget ligula auctor, non tempor massa sodales. Ut rutrum elit id accumsan bibendum. Vivamus at tincidunt eros, sed porttitor est. Curabitur ultricies congue aliquam.',
-        },
-        {
-          from: 'me',
-          photo: photoShepard,
-          time: '11:05 PM',
-          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ornare luctus dui.',
-        },
-        {
-          from: 'friend',
-          photo: photoAshley,
-          time: '11:07 PM',
-          text: 'Lorem ipsum dolor.',
-        }],
-
-      attachment: {
-        photos: {
-          count: '203',
-        },
-        videos: {
-          count: '12',
-        },
-        files: {
-          count: '23',
-        },
-        audio: {
-          count: '10',
-        },
-        links: {
-          count: '238',
-        },
-        voice: {
-          count: '19',
-        },
-      }
-    }
+    AshleyWilliams: chatAshleyWilliams,
   }
 }
 
