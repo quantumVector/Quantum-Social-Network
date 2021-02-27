89034dd0688c3bbbcdd455b69475481c1805a191
quantumVector <payned@mail.ru>
2021-02-27 06:31:37 +0300

    Распределение кода в FriendsPageContainer и FriendsPage для сохранения чистоты функций

--- src/components/FriendsPage/FriendsPage.jsx
+++ src/components/FriendsPage/FriendsPage.jsx
@@ -1,61 +1,43 @@
 import React from 'react';
 import classes from './FriendsPage.module.css';
-import axios from 'axios';
 
-class FriendsPage extends React.Component {
-  componentDidMount = () => {
-    axios.get(`http://backend-quantum-social-network/scripts/get_friends_list.php?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-      console.log(response)
-      this.props.setFriends(response.data.friends);
-      this.props.setTotalFriendsCount(response.data.totalCount)
-    });
+const FriendsPage = (props) => {
+  let pagesCount = Math.ceil(props.totalFriendsCount / props.pageSize);
+  let pages = [];
+
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i);
   }
 
-  onPageChanged = (pageNumber) => {
-    this.props.setCurrentPage(pageNumber);
-    axios.get(`http://backend-quantum-social-network/scripts/get_friends_list.php?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
-      this.props.setFriends(response.data.friends);
-    });
-  }
-
-  render = () => {
-    let pagesCount = Math.ceil(this.props.totalFriendsCount / this.props.pageSize);
-    let pages = [];
-
-    for (let i = 1; i <= pagesCount; i++) {
-      pages.push(i);
-    }
-
-    return (
-      <div className={classes.friends}>
-        <div className={classes.selectPage}>
-          {
-            pages.map(p => {
-              return <span className={this.props.currentPage === p && classes.selected} onClick={(e) => { this.onPageChanged(p) }}>{p}</span>
-            })
-          }
-        </div>
+  return (
+    <div className={classes.friends}>
+      <div className={classes.selectPage}>
         {
-          this.props.friends.map(friend => {
-            return (
-              <div className={classes.friend} key={friend.id}>
-                <img src={friend.photo} className={classes.photo} alt="icon" />
-                <div className={classes.info}>
-                  <div className={classes.name}>{friend.name}</div>
-                  <div className={classes.status}>{friend.status}</div>
-                  <div className={classes.message}>Write message</div>
-                </div>
-                <div className={classes.actions}>
-                  <div className={classes.unfriend}
-                    onClick={() => { this.props.unfriends(friend.id) }}>Unfriend</div>
-                </div>
-              </div>
-            )
+          pages.map(p => {
+            return <span className={props.currentPage === p && classes.selected} onClick={(e) => { props.onPageChanged(p) }}>{p}</span>
           })
         }
       </div>
-    )
-  }
+      {
+        props.friends.map(friend => {
+          return (
+            <div className={classes.friend} key={friend.id}>
+              <img src={friend.photo} className={classes.photo} alt="icon" />
+              <div className={classes.info}>
+                <div className={classes.name}>{friend.name}</div>
+                <div className={classes.status}>{friend.status}</div>
+                <div className={classes.message}>Write message</div>
+              </div>
+              <div className={classes.actions}>
+                <div className={classes.unfriend}
+                  onClick={() => { props.unfriends(friend.id) }}>Unfriend</div>
+              </div>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
 }
 
 export default FriendsPage;
--- src/components/FriendsPage/FriendsPageContainer.jsx
+++ src/components/FriendsPage/FriendsPageContainer.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import FriendsPage from './FriendsPage.jsx';
+import axios from 'axios';
 import { unfriendActionCreator, setFriendsActionCreator, setCurrentPageCreator, setTotalFriendsCountCreator } from '../../redux/friendsReducer';
+
+class FriendsPageContainer extends React.Component {
+  componentDidMount = () => {
+    axios.get(`http://backend-quantum-social-network/scripts/get_friends_list.php?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+      this.props.setFriends(response.data.friends);
+      this.props.setTotalFriendsCount(response.data.totalCount)
+    });
+  }
+
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    axios.get(`http://backend-quantum-social-network/scripts/get_friends_list.php?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+      this.props.setFriends(response.data.friends);
+    });
+  }
+
+  render = () => {
+    return <FriendsPage totalFriendsCount={this.props.totalFriendsCount}
+      pageSize={this.props.pageSize}
+      currentPage={this.props.currentPage}
+      onPageChanged={this.onPageChanged}
+      friends={this.props.friends}
+      unfriends={this.props.unfriends} />
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
@@ -29,6 +55,6 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-const ChatContainer = connect(mapStateToProps, mapDispatchToProps)(FriendsPage);
+const ChatContainer = connect(mapStateToProps, mapDispatchToProps)(FriendsPageContainer);
 
 export default ChatContainer;
